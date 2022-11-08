e1a6ec7a1e6f4eba92144452b5522ed0bc175f35
jorge <engjorgedantas@gmail.com>
2022-11-08 20:28:29 -0300

    Adjust imports

--- lib/modules/movies/data/cache/movies_cache_data_source.dart
+++ lib/modules/movies/data/cache/movies_cache_data_source.dart
@@ -1,5 +1,5 @@
-import 'package:movie_app/modules/movies/domain/model/details/movie_details_model.dart';
-import 'package:movie_app/modules/movies/domain/model/movie/movie_model.dart';
+import '../../domain/model/details/movie_details_model.dart';
+import '../../domain/model/movie/movie_model.dart';
 
 abstract class MoviesCacheDataSource {
   Future<void> saveMovieList(List<MovieModel> movieList);
